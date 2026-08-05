06846024165829062162ebdc1168568aaf345a54
Mirco Ick <mircoick@web.de>
2026-08-05 13:29:45 +0200

    Koordiatensystem der Stepmotoren MotorDriver Class

--- code/main.py
+++ code/main.py
@@ -1,11 +1,24 @@
-from step_motor import MotorDriver,step_motor_conf
+from step_motor import MotorDriver,MotorDriverConf
+import random as r
 
-meine_config = step_motor_conf(
-    PIN_ENA=18,
-    PIN_IN1=22,
-    PIN_IN2=23
-    )
+
+meine_config = MotorDriverConf()
+
+
 
 cam = MotorDriver(config=meine_config)
 #cam.auto_test()
-cam.self_test()
+#cam.self_test()
+#cam.step_impulse(5,"motor1")
+
+
+coords = []
+
+for _ in range(4):
+    coords.append(r.randint(-10, 10))
+    
+#print(coords)
+cam.bresenham_step(coords[0],coords[1],coords[2],coords[3],.01)
+#cam.bresenham_step(-5,0,6,-4,.25)
+
+cam.stop()
--- code/step_motor.py
+++ code/step_motor.py
@@ -4,79 +4,181 @@ import numpy as np
 from typing import Literal
 from pydantic import Field, BaseModel
 import matplotlib.pyplot as plt
+import pybresenham
 
-class step_motor_conf(BaseModel):
+class MotorDriverConf(BaseModel):
     '''
     Mit dieser Klasse werden die Stepper-Motoren über ihre Einstellmöglichkeiten konfiguriert.
     Die Werte der Parameter können seperat eingesehen werden.
     '''
 
-    PIN_ENA: int = Field(ge = 0, le = 26, default= 18)
-    '''Der Enable Pin über dem auch die Pulsweitermodulation erfolgt.'''
-    PIN_IN1: int = Field(ge = 0, le = 26, default= 22)
-    '''Bei Aktivierung Vorwärtslauf.'''
-    PIN_IN2: int = Field(ge = 0, le = 26, default= 23)
-    '''Bei Aktivierung Rückwärtslauf.'''
+    PIN_ENA_EN1: int = Field(ge = 0, le = 27, default= 18)
+    '''Der Enable Pin über dem auch die Pulsweitermodulation erfolgt von Motor 1.'''
+    PIN_IN1_EN1: int = Field(ge = 0, le = 27, default= 22)
+    '''Bei Aktivierung Vorwärtslauf Motor 1.'''
+    PIN_IN2_EN1: int = Field(ge = 0, le = 27, default= 23)
+    '''Bei Aktivierung Rückwärtslauf Motor 1.'''
+    
+    PIN_ENA_EN2: int = Field(ge = 0, le = 27, default= 19)
+    '''Der Enable Pin über dem auch die Pulsweitermodulation erfolgt von Motor 2.'''
+    PIN_IN1_EN2: int = Field(ge = 0, le = 27, default= 17)
+    '''Bei Aktivierung Vorwärtslauf Motor 2.'''
+    PIN_IN2_EN2: int = Field(ge = 0, le = 27, default= 27)
+    '''Bei Aktivierung Rückwärtslauf Motor 2.'''
 
 
 class MotorDriver():
     '''
     Diese Klasse wird verwendet um die Stepper-Motoren zu initialisieren und sie anzusprechen.
     '''
-    def __init__(self, config: step_motor_conf):
+    def __init__(self, config: MotorDriverConf):
         '''Initialisierung der Klasse'''
         self.params = config
         self.h = lgpio.gpiochip_open(0)
+        self.motors = {
+            "motor1": {
+                "en": self.params.PIN_ENA_EN1,
+                "in1": self.params.PIN_IN1_EN1,
+                "in2": self.params.PIN_IN2_EN1
+            },
+            "motor2": {
+                "en": self.params.PIN_ENA_EN2,
+                "in1": self.params.PIN_IN1_EN2,
+                "in2": self.params.PIN_IN2_EN2
+            }
+        }
 
-        lgpio.gpio_claim_output(self.h, self.params.PIN_IN1)
-        lgpio.gpio_claim_output(self.h, self.params.PIN_IN2)
-        lgpio.gpio_claim_output(self.h, self.params.PIN_ENA)
+        for motor_name, pins in self.motors.items():
+            lgpio.gpio_claim_output(self.h, pins["en"])
+            lgpio.gpio_claim_output(self.h, pins["in1"])
+            lgpio.gpio_claim_output(self.h, pins["in2"])
+            
+        for motor_name, pins in self.motors.items():
+            lgpio.gpio_write(self.h, pins["en"],0)
+            lgpio.gpio_write(self.h, pins["in1"],0)
+            lgpio.gpio_write(self.h, pins["in2"],0)
 
-    def set_direction(self, direction: str):
-        """Setzt die Drehrichtung des Motors."""
+    def set_direction(self, direction: str, motor: str):
+        """Setzt die Drehrichtung des wählbaren Motors."""
+        if motor not in self.motors:
+            print(f"Fehler: {motor} existiert nicht.")
+            return
+        
+        pins = self.motors[motor]
+
         if direction == "forward":
-            lgpio.gpio_write(self.h, self.params.PIN_IN1, 1)
-            lgpio.gpio_write(self.h, self.params.PIN_IN2, 0)
+            lgpio.gpio_write(self.h, pins["in1"], 1)
+            lgpio.gpio_write(self.h, pins["in2"], 0)
         elif direction == "backward":
-            lgpio.gpio_write(self.h, self.params.PIN_IN1, 0)
-            lgpio.gpio_write(self.h, self.params.PIN_IN2, 1)
+            lgpio.gpio_write(self.h, pins["in1"], 0)
+            lgpio.gpio_write(self.h, pins["in2"], 1)
         elif direction == "stop":
-            lgpio.gpio_write(self.h, self.params.PIN_IN1, 0)
-            lgpio.gpio_write(self.h, self.params.PIN_IN2, 0)
+            lgpio.gpio_write(self.h, pins["in1"], 0)
+            lgpio.gpio_write(self.h, pins["in2"], 0)
 
-    def auto_test(self):
-        print("Start Autotest")
-
-        self.set_direction("forward")
-
-        for speed in range(100,-1,-1):
-            lgpio.tx_pwm(self.h,self.params.PIN_ENA,1000,speed)
-            print(f"Vorwärtslauf bei {speed}% der Geschwindigkeit")
-            sleep(.5)
-
-        sleep(.5)
-        self.set_direction("stop")
-        sleep(1)
-
-        self.set_direction("backward")
-
-        speed = 20
-
-        for r in range(6):
-            lgpio.tx_pwm(self.h,self.params.PIN_ENA,1000,speed*r)
-            print(f"Rückwärtslauf bei {speed*r}% der Geschwindkeit")
-            sleep(1)
-
-        self.set_direction("stop")
-        print("Ende Autotest")
-
-    def self_test(self):
+    def stop(self):
+        self.set_direction("stop","motor1")
+        self.set_direction("stop","motor2")
+        
+    def pwm_test(self):
         '''Hier können eigene Werte eingegeben werden. Bei einer Eingabe für die Laufrichtung des Motors wird die Schleife abgebrochen.'''
-        direction = input("forward/ backward?	")
+        direction = input("forward/ backward:	")
+        motor = input("motor1/ motor2:	")
         speed = .1
+        pins = self.motors[motor]
         while speed > 0:
             speed = int(input("Bitte die Geschwindigkeit in Prozent angeben: "))
-            self.set_direction(direction)
-            lgpio.tx_pwm(self.h,self.params.PIN_ENA,1000,speed)
+            self.set_direction(direction,motor)
+            lgpio.tx_pwm(self.h,pins["en"],1000,speed)
         print("Stop")
-        self.set_direction("stop")
+        self.set_direction("stop",motor)
+
+    def start_pos(self,x0,y0,sec):
+        
+        print("\nInitialisierung X (Startposition x=0):")
+        if x0 > 0:
+            for i in range(0,x0):
+                self.step_impulse("forward",.5,"motor1")
+                sleep(sec)
+                print(f"i motor1 vorwärts",i+1,x0)
+            self.x_old = x0
+            
+        elif x0 < 0:
+            for i in range(0,x0,-1):
+                self.step_impulse("backward",.5,"motor1")
+                sleep(sec)
+                print("i motor1 Rückwärts",i-1,x0)
+            self.x_old = x0
+        
+        else:
+            self.x_old = x0
+        print(f"\nEnde Initialisierung x = {self.x_old,self.y_old}\n")
+            
+        print("Initialisierung Y (Startposition y=0):")
+        if y0 > 0:
+            for i in range(0,y0):
+                self.step_impulse("forward",.5,"motor2")
+                sleep(sec)
+                print("i motor2 vorwärts",i+1,y0)
+            self.y_old = y0
+            
+        elif y0 < 0:
+            for i in range(0,y0,-1):
+                self.step_impulse("backward",.5,"motor2")
+                sleep(sec)
+                print("i motor2 Rückwärts",i-1,y0)
+            self.y_old = y0
+        else:
+            self.y_old = y0
+            
+        print(f"\nEnde Initialisierung y = {self.x_old,self.y_old}\n")
+        
+    def step_impulse(self, direction:str,impulse: int,motor:str):
+        '''Ein Step Impulse der mit der Zeitangabe gesteuert werden kann.'''
+        if motor not in self.motors:
+            print(f"Fehler: {motor} existiert nicht.")
+            return
+        
+        self.set_direction(direction,motor)
+        
+        pins = self.motors[motor]
+        lgpio.tx_pwm(self.h,pins["en"],1000,100)
+        sleep(impulse)
+        lgpio.tx_pwm(self.h,pins["en"],1000,0)
+        
+    def bresenham_step(self,x0:int,y0:int,x1:int,y1:int,sec):
+        steps = list(pybresenham.line(x0,y0,x1,y1))
+        print(steps)
+        self.x_old = None
+        self.y_old = None
+        s=0
+        self.start_pos(x0,y0,sec)
+        for x,y in steps:
+            
+            if x > self.x_old:
+                self.step_impulse("forward",.5,"motor1")
+                self.x_old = x
+                sleep(sec)
+                print("Einzelschritt x Vorwärts",self.x_old,self.y_old)
+            elif x < self.x_old:
+                self.step_impulse("backward",.5,"motor1")
+                self.x_old = x
+                sleep(sec)
+                print("Einzelschritt x Rückwärts",self.x_old,self.y_old)
+            if y > self.y_old:
+                self.step_impulse("forward",.5,"motor2")
+                self.y_old = y
+                sleep(sec)
+                print("Einzelschritt y Vorwärts",self.x_old,self.y_old)
+            elif y < self.y_old:
+                self.step_impulse("backward",.5,"motor2")
+                self.y_old = y
+                sleep(sec)
+                print("Einzelschritt y Rückwärts",self.x_old,self.y_old)
+            
+            print(f"\nNächster Gesamtschritt {x,y}\t{steps[s]}")
+            s=s+1
+            
+        print("\nENDE")
+        
+        
